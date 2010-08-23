7b7ae9229e1ad8e02690796368d4762235d211fd
Andrew Wooster <wooster_spam@nextthing.org>
2010-08-22 21:30:51 -0700

    Some intrmediate work on writing binary plists.

--- biplist/__init__.py
+++ biplist/__init__.py
@@ -1,17 +1,33 @@
 from collections import namedtuple
 from cStringIO import StringIO
 import datetime
+import math
 import plistlib
 from struct import pack, unpack
+import time
+
+__all__ = [
+    'Uid', 'Data', 'readPlist', 'writePlist', 'readPlistFromString',
+    'writePlistToString', 'InvalidPlistException', 'NotBinaryPlistException'
+]
+
+apple_reference_date_offset = 978307200
 
 class Uid(int):
     """Wrapper around integers for representing UID values."""
     pass
 
+class Data(str):
+    """Wrapper around str types for representing Data values."""
+    pass
+    
+
 class InvalidPlistException(Exception):
+    """Raised when the plist is incorrectly formatted."""
     pass
 
 class NotBinaryPlistException(Exception):
+    """Raised when a binary plist was expected but not encountered."""
     pass
 
 def readPlist(pathOrFile):
@@ -66,17 +82,8 @@ def is_stream_binary_plist(stream):
     else:
         return False
 
-class PlistWriter(object):
-    def __init__(self, fileOrStream, rootObject):
-        self.file = fileOrStream
-        self.writeRoot(rootObject)
-    def writeRoot(self, rootObject):
-        self.uniqueObjects = []
-        self.objectCount = 0
-        pass
-
 PlistTrailer = namedtuple('PlistTrailer', 'offsetSize, objectRefSize, offsetCount, topLevelObjectNumber, offsetTableOffset')
-
+PlistByteCounts = namedtuple('PlistByteCounts', 'boolBytes, intBytes, realBytes, dateBytes, dataBytes, stringBytes, uidBytes, arrayBytes, setBytes, dictBytes')
 
 class PlistReader(object):
     file = None
@@ -129,6 +136,7 @@ class PlistReader(object):
                 offset_i += 1
             self.setCurrentOffsetToObjectNumber(self.trailer.topLevelObjectNumber)
             self.root = self.readObject()
+            print self.offsets
         except TypeError, e:
             raise InvalidPlistException(e)
         print "root is:", self.root
@@ -286,15 +294,16 @@ class PlistReader(object):
         return data.decode('utf-16-be')
     
     def readDate(self):
+        global apple_reference_date_offset
         result = unpack(">d", self.contents[self.currentOffset:self.currentOffset+8])[0]
-        result = datetime.datetime.utcfromtimestamp(result + 978307200)
+        result = datetime.datetime.utcfromtimestamp(result + apple_reference_date_offset)
         self.currentOffset += 8
         return result
     
     def readData(self, length):
         result = self.contents[self.currentOffset:self.currentOffset+length]
         self.currentOffset += length
-        return result
+        return Data(result)
     
     def readUid(self, length):
         return Uid(self.readInteger(length+1))
@@ -311,24 +320,217 @@ class PlistReader(object):
 
 class PlistWriter(object):
     file = None
-    binaryObjects = None
+    byteCounts = None
     offsets = None
+    serializedUniques = None
     trailer = None
     uniques = None
+    uniquePositions = None
+    references = 0
+    header = 'bplist00'
+    trailer_size = 32
     
     def __init__(self, file):
         self.reset()
         self.file = file
 
     def reset(self):
+        self.byteCounts = PlistByteCounts(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
         self.offsets = []
-        self.trailer = None
+        self.serializedUniques = ''
+        self.trailer = PlistTrailer()
         self.uniques = []
+        self.uniquePositions = []
+        self.references = 0
         
     def writeRoot(self, root):
-        result = 'bplist00'
+        """
+        Strategy is:
+        - write header
+        - write objects
+        - computer object reference length
+        - write object reference positions
+        - writer trailer
+        """
+        result = self.header
+        
+        
+        
+        
         self.computeUniquesAndOffsets(root)
+        
+        self.trailer.objectRefSize = self.intSize(self.references)
+        ref_size = self.trailer.objectRefSize * self.references
+        #!! trailer_size is uneccesary?
+        file_size = sum(self.byteCounts) + ref_size + len(self.header) + self.trailer_size
+        # Should write offsets after data, so shouldn't require longer length.
+        self.trailer.offsetSize = self.intSize(len(file_size))
+        self.trailer.offsetCount = len(self.uniques)
+        self.trailer.topLevelObjectNumber = 0
+        
+        result = self.serializeObject(root, result)
+        self.trailer.offsetTableOffset = len(result)
+        result += self.serializeOffsets(self.trailer.offsetSize)
+        result += pack('!xxxxxxBBQQQ', *trailer)
+        
         self.file.write(result)
+    
+    def intSize(self, obj):
+        #!! Should actually calculate size required.
+        return 8
+
+    def serializeOffsets(self, offsetSize):
+        result = ''
+        for offset in self.offsets:
+            if offsetSize == 1:
+                result += pack('>B', offset)
+            elif offsetSize == 2:
+                result += pack('>H', offset)
+            elif offsetSize == 4:
+                result += pack('>L', offset)
+            elif offsetSize == 8:
+                result += pack('>q', offset)
+        return result
+
+    def serializeObject(self, obj, result):    
+        def proc_variable_length(format, length):
+            result = ''
+            if length > 0b1110:
+                result += pack('!B', (format << 4) | 0b1111)
+                result += self.binaryInt(length)
+            else:
+                result += pack('!B', (format << 4) | length)
+            return result
+        def proc_unique(unique):
+            result = ''
+            if isinstance(unique, (str, unicode, Data)):
+                raise NotImplementedError("hmmm")
+            else:
+                raise NotImplementedError("hmmm")
+            return result
+        
+        if type(obj) == bool:
+            if obj is False:
+                result += pack('!B', b00001000)
+            else:
+                result += pack('!B', b00001001)
+        elif isinstance(obj, Uid):
+            size = self.intSize(obj)
+            result += pack('!B', (0b1000 << 4) | size - 1)
+            result += self.binaryInt(Uid)
+        elif isinstance(obj, (int, long)):
+            bytes = self.intSize(obj)
+            root = math.log(bytes, 2)
+            result += pack('!B', (0b0001 << 4) | root)
+            result += self.binaryInt(obj)
+        elif isinstance(obj, float):
+            # just use doubles
+            result += pack('!B', (0b0010 << 4) | 3)
+            result += self.binaryReal(obj)
+        elif isinstance(obj, datetime.datetime):
+            timestamp = time.mktime(obj.timetuple())
+            timestamp -= apple_reference_date_offset
+            result += pack('!B', 0b00110011)
+            result += pack('!d', float(timestamp))
+        elif isinstance(obj, Data):
+            result += proc_variable_length(0b0100, len(obj))
+            result += obj
+        elif isinstance(obj, (str, unicode)):
+            if isinstance(obj, unicode):
+                bytes = obj.encode('utf-16be')
+                result += proc_variable_length(0b0110, len(bytes))
+            else:
+                bytes = obj
+                result += proc_variable_length(0b0101, len(bytes))
+            result += bytes
+        elif isinstance(obj, set):
+            result += proc_variable_length(0b1100, len(obj))
+            for unique in obj:
+                result += proc_unique(unique)
+        elif isinstance(obj, (list, tuple)):
+            result += proc_variable_length(0b1010, len(obj))
+            for unique in obj:
+                result += proc_unique(unique)
+        elif isinstance(obj, dict):
+            result += proc_variable_length(0b1101, len(obj))
+            keys = []
+            values = []
+            for key, value in obj.iteritems():
+                keys.append(key)
+                values.append(value)
+            for key in keys:
+                result += proc_unique(key)
+            for value in values:
+                result += proc_unique(value)
+        return result
+    
+    def binaryReal(self, obj):
+        # just use doubles
+        result = pack('>d', obj)
+        return result
+    
+    def binaryInt(self, obj):
+        result = ''
+        if bytes == 1:
+            result += pack('>B', obj)
+        elif bytes == 2:
+            result += pack('>H', obj)
+        elif bytes == 4:
+            result += pack('>L', obj)
+        elif bytes == 8:
+            result += pack('>q', obj)
+        else:
+            #!! Uh... what to do here?
+            raise NotImplementedError("Not sure how to do this yet.")
+        return result
 
     def computeUniquesAndOffsets(self, obj):
-        pass
+        def proc_size(size):
+            if size > 0b1110:
+                size += self.intSize(size)
+            return size
+        
+        if type(obj) == bool:
+            self.byteCounts.boolBytes += 1
+        elif isinstance(obj, Uid):
+            size = self.intSize(obj)
+            self.byteCounts.uidBytes += 1 + size
+        elif isinstance(obj, (int, long)):
+            size = self.intSize(obj)
+            self.byteCounts.intBytes += 1 + size
+        elif isinstance(obj, (float)):
+            size = self.floatSize(obj)
+            self.byteCounts.realBytes += 1 + size
+        elif isinstance(obj, datetime.datetime):
+            self.byteCounts.dateBytes += 2
+        elif isinstance(obj, Data):
+            if obj not in self.uniques:
+                size = proc_size(len(obj))
+                self.byteCounts.dataBytes += 1 + size
+                self.uniques.append(obj)
+        elif isinstance(obj, (str, unicode)):
+            if obj not in self.uniques:
+                size = proc_size(len(obj))
+                self.byteCounts.stringBytes += 1 + size
+                self.uniques.append(obj)
+        elif isinstance(obj, set):
+            size = proc_size(len(obj))
+            self.byteCounts.setBytes += 1 + size
+            for value in obj:
+                self.references += 1
+                self.computeUniquesAndOffsets(value)
+        elif isinstance(obj, (list, tuple)):
+            size = proc_size(len(obj))
+            self.byteCounts.arrayBytes += 1 + size
+            for value in obj:
+                self.references += 1
+                self.computeUniquesAndOffsets(value)
+        elif isinstance(obj, dict):
+            size = proc_size(len(obj))
+            self.byteCounts.dictBytes += 1 + size
+            for key, value in obj.iteritems():
+                self.references += 2
+                self.computeUniquesAndOffsets(key)
+                self.computeUniquesAndOffsets(value)
+        else:
+            raise InvalidPlistException("Unknown object type.")
