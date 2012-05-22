4c719645ff2ec5e08f78b1e27e008eb06cca287e
Kevin Kelley <kelleyk@kelleyk.net>
2012-05-21 17:27:32 -0700

    Tweaked setup and added tox-file.

--- setup.py
+++ setup.py
@@ -12,11 +12,9 @@ import sys
 
 major, minor, micro, releaselevel, serial = sys.version_info
 
-if major == 2 and minor < 6:
-    print("Python >= 2.6 is required to use this module.")
-    sys.exit(1)
-elif major >= 3:
-    print("There is no support for Python 3 yet in this module.")
+if major <= 1 or (major == 2 and minor < 6) or (major == 3 and minor < 2):
+    # N.B.: Haven't tested with older py3k versions.
+    print('This module supports Python 2 >= 2.6 and Python 3 >= 3.2.')
     sys.exit(1)
 
 author = 'Andrew Wooster'
@@ -27,18 +25,19 @@ desc = 'biplist is a library for reading/writing binary plists.'
 setup(
     name = 'biplist',
     version = version,
-    url = 'https://github.com/wooster/biplist',
-    download_url = 'https://github.com/wooster/biplist/downloads/biplist-0.4.tar.gz',
+    url = 'https://github.com/kelleyk/py3k-biplist',
     license = 'BSD',
     description = desc,
     long_description = 
     """`biplist` is a binary plist parser/generator for Python.
 
+(Ported to Python 3 by Kevin Kelley <kelleyk@kelleyk.net>.)
+
 Binary Property List (plist) files provide a faster and smaller serialization
 format for property lists on OS X. This is a library for generating binary
 plists which can be read by OS X, iOS, or other clients.
 
-This module requires Python 2.6 or higher.""",
+This module requires Python 2.6 or higher or Python 3.2 or higher.""",
     author = author,
     author_email = email,
     packages = find_packages(),
@@ -55,6 +54,6 @@ This module requires Python 2.6 or higher.""",
     ],    
     setup_requires = ['nose', 'coverage'],
     test_suite = 'nose.collector',
-    install_requires = [
-    ],
+    install_requires = ['six'],
+    requires = ['six'],
 )
